604cbe3377a5b31ccc675ad4050b6ae2edbc976a
eclipse24 <150936060+neonred226@users.noreply.github.com>
2025-08-29 15:07:37 +0500

    pixelProject
    Signed-off-by: eclipse24 <150936060+neonred226@users.noreply.github.com>

--- script.sh
+++ script.sh
@@ -9,7 +9,7 @@ echo "Repo init success"
 echo "=================="
 
 # Local manifests
-git clone https://github.com/neonred226/android-rom-build -b pixel .repo/local_manifests
+git clone https://github.com/neonred226/android-rom-build -b PixelP .repo/local_manifests
 echo "============================"
 echo "Local manifest clone success"
 echo "============================"
