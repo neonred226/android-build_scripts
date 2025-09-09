ddc2256d866cb3b72b98c83e5f664702bc94af2e
eclipse24 <150936060+neonred226@users.noreply.github.com>
2025-09-09 13:51:11 +0500

    set script for derp A16
    Signed-off-by: eclipse24 <150936060+neonred226@users.noreply.github.com>

--- script.sh
+++ script.sh
@@ -3,13 +3,13 @@
 rm -rf .repo/local_manifests/
 
 # repo init rom
-repo init -u https://github.com/neonred226/manifest_derp.git -b 15.2 --git-lfs
+repo init -u https://github.com/neonred226/android_manifest.git -b 16 --git-lfs
 echo "=================="
 echo "Repo init success"
 echo "=================="
 
 # Local manifests
-git clone https://github.com/neonred226/android-rom-build -b derp-15.2 .repo/local_manifests
+git clone https://github.com/BaranAspect-Development/android_local_manifests .repo/local_manifests
 echo "============================"
 echo "Local manifest clone success"
 echo "============================"
@@ -30,4 +30,4 @@ export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 
 
 echo "======= Export Done ======"
-lunch lineage_topaz-bp1a-userdebug && mka derp
+lunch lineage_topaz-bp2a-userdebug && mka derp
